77e788ae356cc08459bb72061b744d49b5309af6
yashjain092000 <yashjain092000@gmail.com>
2020-11-11 21:57:16 +0530

    add login signup and firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,48 @@
 import 'package:flutter/material.dart';
-import 'screens/welcome_screen.dart';
-// import 'screens/login_screen.dart';
-// import 'screens/registration_screen.dart';
-import 'screens/dashboard.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'auth_screen.dart';
+import 'chat_screen.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:page_transition/page_transition.dart';
 
-void main() => runApp(Khojpur());
+// void main() => runApp(Khojpur());
 
-class Khojpur extends StatelessWidget {
+// class Khojpur extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: StreamBuilder(
+//           stream: FirebaseAuth.instance.onAuthStateChanged,
+//           builder: (ctx, userSnapShot) {
+//             if (userSnapShot.hasData) {
+//               return ChatScreen();
+//             } else {
+//               return AuthScreen();
+//             }
+//           }),
+//     );
+//   }
+// }
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Clean Code',
+        home: AnimatedSplashScreen(
+            duration: 3000,
+            splash: Icons.home,
+            nextScreen: MainScreen(),
+            splashTransition: SplashTransition.fadeTransition,
+            pageTransitionType: PageTransitionType.scale,
+            backgroundColor: Colors.blue));
+  }
+}
+
+class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,7 +53,7 @@ class Khojpur extends StatelessWidget {
             if (userSnapShot.hasData) {
               return ChatScreen();
             } else {
-              return WelcomeScreen();
+              return AuthScreen();
             }
           }),
     );
