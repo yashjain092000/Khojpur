deacc699cf36c8ad0e66bf180c67db589097f9da
yashjain092000 <yashjain092000@gmail.com>
2020-11-17 13:32:21 +0530

    add logout button in dashboard

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -112,6 +112,33 @@ class _DashboardPageState extends State<DashboardPage> {
               },
             ),
             title: Text(widget.title),
+            actions: [
+              DropdownButton(
+                icon: Icon(
+                  Icons.more_vert,
+                  color: Theme.of(context).primaryIconTheme.color,
+                ),
+                items: [
+                  DropdownMenuItem(
+                    child: Container(
+                      child: Row(
+                        children: <Widget>[
+                          Icon(Icons.exit_to_app),
+                          SizedBox(width: 8),
+                          Text('Logout'),
+                        ],
+                      ),
+                    ),
+                    value: 'logout',
+                  ),
+                ],
+                onChanged: (itemIdentifier) {
+                  if (itemIdentifier == 'logout') {
+                    FirebaseAuth.instance.signOut();
+                  }
+                },
+              ),
+            ],
           ),
           body: Center(
             child: Column(
