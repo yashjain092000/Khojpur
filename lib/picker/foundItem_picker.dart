1027d8d06750fc88087a2b2453f74076c0b2754d
yashjain092000 <yashjain092000@gmail.com>
2020-11-26 11:23:58 +0530

    some ui changes

--- lib/picker/foundItem_picker.dart
+++ lib/picker/foundItem_picker.dart
@@ -2,9 +2,6 @@ import 'package:flutter/material.dart';
 import 'dart:io';
 import 'package:image_picker/image_picker.dart';
 import 'package:firebase_storage/firebase_storage.dart';
-//import 'package:flutter/material.dart';
-//import 'package:image_picker/image_picker.dart';
-//import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class FoundItemPicker extends StatefulWidget {
